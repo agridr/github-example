7e16b7700b8ba43a7f15c3addbbdcd85aa58e951
nehaOffice <129371381+nehaOffice@users.noreply.github.com>
2023-03-30 16:40:55 +0530

    Update CountdownTimer.js

--- src/CountdownTimer.js
+++ src/CountdownTimer.js
@@ -7,7 +7,7 @@ const ExpiredNotice = () => {
     return (
         <div className="expired-notice">
             <span>Expired!!!</span>
-            <p>Please select a future date and time.</p>
+            <p>Please select a future date time .... </p>
         </div>
     );
 };
